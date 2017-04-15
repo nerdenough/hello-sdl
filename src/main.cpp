b77a085ffea1c958c848b5199b7861fe84ecc88e
Brendan Goodenough <brendan@goodenough.nz>
2017-04-15 18:22:00 +1200

    Show a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,61 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window *gWindow = NULL;
+SDL_Surface *gScreenSurface = NULL;
+SDL_Surface *gHelloWorld = NULL;
+
+bool init() {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL failed to initialise! SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (gWindow == NULL) {
+    printf("Unable to create window! SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  gScreenSurface = SDL_GetWindowSurface(gWindow);
+  return true;
+}
+
+bool loadMedia() {
+  return true;
+}
+
+void close() {
+  SDL_FreeSurface(gScreenSurface);
+  gScreenSurface = NULL;
+
+  SDL_DestroyWindow(gWindow);
+  gWindow = NULL;
+
+  SDL_Quit();
+}
+
 int main(int argc, char *args[]) {
-  printf("%s\n", "Hello, world!");
+  if (!init()) {
+    printf("Initialisation failed!\n");
+    return 0;
+  }
+
+  if (!loadMedia()) {
+    printf("Load media failed!\n");
+    return 0;
+  }
+
+  SDL_UpdateWindowSurface(gWindow);
+  SDL_Delay(2000);
+
+  close();
+  return 0;
 }
