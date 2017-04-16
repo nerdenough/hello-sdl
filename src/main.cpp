18be586451db139a47b1687ce1cd4fd9e1255011
Brendan Goodenough <brendan@goodenough.nz>
2017-04-16 13:32:21 +1200

    Render textures to the screen using hardware acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 bool init();
-SDL_Surface *loadSurface(std::string path);
+SDL_Texture *loadTexture(std::string path);
 bool loadMedia();
 void loop();
 void close();
 
 SDL_Window *gWindow = NULL;
-SDL_Surface *gScreenSurface = NULL;
+SDL_Renderer *gRenderer = NULL;
+SDL_Texture *gTexture = NULL;
 
 bool init() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -27,33 +28,44 @@ bool init() {
     return false;
   }
 
-  int imgFlags = IMG_INIT_PNG;
-  if (!(IMG_Init(imgFlags) & imgFlags)) {
-    printf("Image flags failed to initialise: SDL_image Error: %s\n", IMG_GetError());
+  gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+  if (gRenderer == NULL) {
+    printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
     return false;
   }
 
-  gScreenSurface = SDL_GetWindowSurface(gWindow);
+  int imgFlags = IMG_INIT_PNG;
+  if (!(IMG_Init(imgFlags) & imgFlags)) {
+    printf("SDL_image could not initialise: SDL_image Error: %s\n", IMG_GetError());
+    return false;
+  }
+
   return true;
 }
 
-SDL_Surface *loadSurface(std::string path) {
+SDL_Texture *loadTexture(std::string path) {
   SDL_Surface *loadedSurface = IMG_Load(path.c_str());
   if (loadedSurface == NULL) {
     printf("Failed to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
     return NULL;
   }
 
-  SDL_Surface *optimisedImage = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
-  if (optimisedImage == NULL) {
-    printf("Unable to optimise image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-    SDL_FreeSurface(loadedSurface);
+  SDL_Texture *newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+  if (newTexture == NULL) {
+    printf("Unable to create texture from surface! SDL Error: %s\n", SDL_GetError());
   }
 
-  return optimisedImage;
+  SDL_FreeSurface(loadedSurface);
+  return newTexture;
 }
 
 bool loadMedia() {
+  gTexture = loadTexture("assets/default.png");
+  if (gTexture == NULL) {
+    printf("Error loading default image\n");
+    return false;
+  }
+
   return true;
 }
 
@@ -68,7 +80,10 @@ void loop() {
       }
     }
 
-    SDL_UpdateWindowSurface(gWindow);
+    // Clear screen, render texture, and update the screen
+    SDL_RenderClear(gRenderer);
+    SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+    SDL_RenderPresent(gRenderer);
   }
 }
 
